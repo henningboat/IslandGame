e56c4f8c3dfe0a7df78bfdaf3a4f8b36d555f7ce
henningboat <talktohenning@gmail.com>
2021-08-14 17:24:16 +0200

    Added DoTween and also TMP

---
+++ Assets/Code/Runtime/GameLogic/PlayerInventory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace IslandGame.GameLogic
+{
+    public class PlayerInventory : MonoBehaviour
+    {
+        public int LogCount;
+
+        public void AddLog()
+        {
+            LogCount++;
+        }
+
+        public void ConsumeLogs(int logs)
+        {
+            LogCount -= logs;
+        }
+    }
+}
